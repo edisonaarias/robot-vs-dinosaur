5c14f0d6280ff3b352bbf95a2e4c25a782a9bef8
unknown <edisonalexanderarias@gmail.com>
2022-05-12 11:42:31 -0700

    added class

---
+++ Battlefield.py
@@ -0,0 +1,16 @@
+from Dinosaur import dinosaur
+from Robot import robot
+
+class battlefield:
+    def __init__(self):
+
+    def run_game(self):
+        pass
+    def display_welcome(self):
+        print('\nWelcome to an epic battle for the ages!\nOnly one side cna win!\n')
+        
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from Battlefield import battlefield
+
+Battlefield_one = battlefield()
+Battlefield_one.run_game()
