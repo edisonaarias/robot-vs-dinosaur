f5b2cf2cdb9a9110f48ff865ff168ea88286c5bc
unknown <edisonalexanderarias@gmail.com>
2022-05-13 11:35:08 -0700

    adding to main file

--- Battlefield.py
+++ Battlefield.py
@@ -17,13 +17,13 @@ Dinosaur = Dinosaur("Head Bash", 400, "robot was hit")
 Robot = Robot("Bone saw", 500, "dinosaur was hit")
 Dinosaur = Dinosaur("Bite", 400, "Wake-up Bevvy")
 
-    def display_welcome(self):
+def display_welcome(self):
         print('\nWelcome to an epic battle for the ages!\nOnly one side cna win!\n')
         
-    def battle_phase(self):
+        def battle_phase(self):
 
-        self.dinosaur.Dinosaur_attack_power(self.dinosaur)
-        self.robot.weapon_attack_power(self.robot)
+            self.dinosaur.Dinosaur_attack_power(self.dinosaur)
+            self.robot.weapon_attack_power(self.robot)
 
         if self.Dinosaur_attack_power is True:
             print("robot attacks")
@@ -36,5 +36,5 @@ Dinosaur = Dinosaur("Bite", 400, "Wake-up Bevvy")
 
 
 
-    def display_winner(self):
-        print( "The winner has been decided")
+        def display_winner(self):
+            print( "The winner has been decided")
